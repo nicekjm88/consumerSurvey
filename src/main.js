5148686cf8c59f54fc772cdb4a60d05779bd10dd
James Kim <5000win@naver.com>
2022-01-25 16:32:56 +0900

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./assets/router.js";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-createApp(App)
-  .use(router)
-  .mount("#app");
