9f88265b38d24360e085e189e8f53887e1cfd9b4
James <5000win@naver.com>
2022-03-15 11:43:00 +0900

    마케팅 조니 피드백 반영

--- src/Page/Account/Result.vue
+++ src/Page/Account/Result.vue
@@ -1,64 +1,71 @@
 <template>
-<div class="bg-sky">
-  <Navigation />
-  <main class="result-area">
-    <section>
-      <h1 class="title">적극적으로 참여해주셔서<br>진심으로 감사합니다.</h1>
+  <div class="bg-sky">
+    <Navigation />
+    <main class="result-area">
+      <section>
+        <h1 class="title">설문에 참여해주셔서<br />감사합니다.</h1>
 
-      <div class="rounded-box rounded-box__wrapper">
-        <h2 class="title">결과 확인 및 설명</h2>
-        <hr />
-        <p>총 {{groupCount.TotalCount}}개를 선택하였습니다.</p>
-        <p>&lt;선택리스트&gt;</p>
-        <ul>
-          <li v-for="(item, idx) in groupCount.Groups" :key="idx"> {{item.Name}} {{item.Count}}개 </li>
-        </ul>
+        <div class="rounded-box rounded-box__wrapper">
+          <h2 class="title">결과 확인 및 설명</h2>
+          <hr />
+          <p>총 {{ groupCount.TotalCount }}개를 선택하였습니다.</p>
+          <p>[선택리스트]</p>
+          <ul class="list list-dot">
+            <li v-for="(item, idx) in groupCount.Groups" :key="idx">
+              {{ item.Name }} {{ item.Count }}개
+            </li>
+          </ul>
 
-        <p>정말 많은 제품등을 소비하고 계시네요.</p>
-        <p>
-          가족 4인 기준에서<br>
-          위 제품을 소비하는 총 금액은 매월 평균 소비 기준 0,000,000원입니다.<br>
-          1년간 꾸준히 사용하신다면 약 00,000,000원입니다.
-        </p>
+          <p>내가 선택한 상품 “애터미 쇼핑몰”에서 찾아보기</p>
 
-        <p>애터미에도 가격도 저렴하고<br>품질도 좋은 제품 등이 있습니다.</p>
+          <router-link to="/AtomyProduct">
+            <div class="d-grid">
+              <button class="btn btn-outline-primary block">
+                선택하신 상품이 애터미 쇼핑몰에도 있어요!
+              </button>
+            </div>
+          </router-link>
 
-        <router-link to="/AtomyProduct">
-          <div class="d-grid">
-            <button class="btn btn-outline-primary block">선택하신 제품이 애터미에도 있어요!</button>
-          </div>
-        </router-link>
-
-        <p>현재 애터미 제품으로 이 물품 등을 구매하신다면<br>매월 소비 금액이 0,000,000원입니다.</p>
-      </div>
-    </section>
-  </main>
-  <router-link to="/Result2">
-    <FixedBtn msg="다음" />
-  </router-link>
-</div>
+          <p>
+            0인 가족 기준<br />
+            애터미 쇼핑몰에서 위 상품 등을 구매하시면<br />
+            매년 소비 금액은 0,000,000원이며<br />
+            누적 포인트(PV)는 0,000,000 PV입니다.<br />
+          </p>
+          <p>
+            누적 포인트에 따라<br />
+            더 많은 혜택을 받고 싶으시다면 아래 버튼을<br />
+            클릭해주세요.
+          </p>
+        </div>
+      </section>
+    </main>
+    <router-link to="/Result2">
+      <FixedBtn msg="다음" />
+    </router-link>
+  </div>
 </template>
 
 <script>
-import Navigation from '@/components/Layout/Navigation.vue'
-import FixedBtn from '@/components/Layout/FixedBtn.vue'
+import Navigation from "@/components/Layout/Navigation.vue";
+import FixedBtn from "@/components/Layout/FixedBtn.vue";
 import useProductsManager from "@/store/products-manager";
-import {computed} from "vue";
+import { computed } from "vue";
 export default {
-  name: 'Result',
+  name: "Result",
   components: {
     Navigation,
-    FixedBtn
+    FixedBtn,
   },
-  setup(){
+  setup() {
     const productsManager = useProductsManager();
     const groupCount = computed(() => productsManager.getSelectedGroupCount());
 
     return {
       groupCount,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -85,7 +92,6 @@ export default {
     margin-bottom: 1rem;
   }
 
-
   .btn {
     border: 2px solid #00b5ef;
     color: #00b5ef;
@@ -94,4 +100,22 @@ export default {
   }
 }
 
+.list {
+  &.list-dot {
+    > li {
+      padding-left: 15px;
+      position: relative;
+
+      &::before {
+        content: "· ";
+        position: absolute;
+        top: 0;
+        left: 5px;
+        font-size: 20px;
+        font-weight: 700;
+        line-height: 1;
+      }
+    }
+  }
+}
 </style>
--- src/Page/SaveData/SaveDataView.vue
+++ src/Page/SaveData/SaveDataView.vue
@@ -1,157 +1,172 @@
 <template>
   <div class="wrap">
-    <Navigation @buttonDeleteClick="handleDelect" @buttonEditClick="handleEdit"/>
+    <Navigation
+      @buttonDeleteClick="handleDelect"
+      @buttonEditClick="handleEdit"
+    />
     <main>
       <section>
         <h1 class="title">{{ item.Name }}님의 자료</h1>
 
         <table class="table table-striped">
-          <caption>최종 자료</caption>
+          <caption>
+            최종 자료
+          </caption>
           <colgroup>
             <col style="width: 50%;" />
           </colgroup>
           <tbody>
-          <tr>
-            <th scope="row">작성일</th>
-            <td>{{ reactResultFormatted.CreateAt }}</td>
-          </tr>
-          <tr>
-            <th scope="row">생년월일</th>
-            <td>{{ reactResultFormatted.BirthDay }}</td>
-          </tr>
-          <tr>
-            <th scope="row">성별</th>
-            <td>{{ reactResultFormatted.Gender }}</td>
-          </tr>
-          <tr>
-            <th scope="row">연령대</th>
-            <td>{{ reactResultFormatted.Age }}</td>
-          </tr>
-          <tr>
-            <th scope="row">가족 수</th>
-            <td>{{ reactResultFormatted.Families }}</td>
-          </tr>
-          <tr>
-            <th scope="row">애터미 제품 사용 유무</th>
-            <td>{{ reactResultFormatted.UseAtomyYn }}</td>
-          </tr>
-          <tr>
-            <th scope="row">전화번호</th>
-            <td>{{ reactResultFormatted.Phone }}</td>
-          </tr>
-<!--          <tr>-->
-<!--            <th scope="row">월 평균 생활소비 금액</th>-->
-<!--            <td>10,000,000원</td>-->
-<!--          </tr>-->
-          <tr>
-            <th scope="row">연 평균 애터미 제품 소비 금액</th>
-            <td>{{ reactResultFormatted.AmountPerYear }}</td>
-          </tr>
-          <tr>
-            <th scope="row">연 획득 총 PV</th>
-            <td>{{ reactResultFormatted.PVPerYear }}</td>
-          </tr>
-          <tr>
-            <th scope="row">평균 수당 지급 횟수</th>
-            <td>{{reactResultFormatted.PayBackCount}}</td>
-          </tr>
-          <tr>
-            <th scope="row">선택상품</th>
-            <td>{{item.ProductNames}}</td>
-          </tr>
+            <tr>
+              <th scope="row">작성일</th>
+              <td>{{ reactResultFormatted.CreateAt }}</td>
+            </tr>
+            <tr>
+              <th scope="row">생년월일</th>
+              <td>{{ reactResultFormatted.BirthDay }}</td>
+            </tr>
+            <tr>
+              <th scope="row">성별</th>
+              <td>{{ reactResultFormatted.Gender }}</td>
+            </tr>
+            <tr>
+              <th scope="row">연령대</th>
+              <td>{{ reactResultFormatted.Age }}</td>
+            </tr>
+            <tr>
+              <th scope="row">가족 수</th>
+              <td>{{ reactResultFormatted.Families }}</td>
+            </tr>
+            <tr>
+              <th scope="row">애터미 제품 사용</th>
+              <td>{{ reactResultFormatted.UseAtomyYn }}</td>
+            </tr>
+            <tr>
+              <th scope="row">전화번호</th>
+              <td>{{ reactResultFormatted.Phone }}</td>
+            </tr>
+            <tr>
+              <th scope="row">연 평균 생활소비 금액</th>
+              <td>{{ reactResultFormatted.AmountPerYear }}</td>
+            </tr>
+            <tr>
+              <th scope="row">연 획득 총 PV</th>
+              <td>{{ reactResultFormatted.PVPerYear }}</td>
+            </tr>
+            <tr>
+              <th scope="row">예상 수당 지급 횟수(최소)</th>
+              <td>약 {{ reactResultFormatted.PayBackCount }}회</td>
+            </tr>
+            <tr>
+              <th scope="row">선택상품</th>
+              <td>{{ item.ProductNames }}</td>
+            </tr>
           </tbody>
         </table>
-        <FixedBtn @click="snsShare('나의 소비생활 알아보기', 'SaveDataView')" type="button" msg="공유하기" />
+        <FixedBtn
+          @click="snsShare('나의 소비생활 알아보기', 'SaveDataView')"
+          type="button"
+          msg="공유하기"
+        />
       </section>
     </main>
   </div>
 </template>
 
 <script>
-import Navigation from '@/components/Layout/Navigation.vue';
-import FixedBtn from '@/components/Layout/FixedBtn.vue';
-import {computed, onBeforeMount, reactive} from "vue";
+import Navigation from "@/components/Layout/Navigation.vue";
+import FixedBtn from "@/components/Layout/FixedBtn.vue";
+import { computed, onBeforeMount, reactive } from "vue";
 import useSurvey from "@/composables/api/survey";
 import router from "@/router";
 import useFormatter from "@/composables/api/utils/formatter";
 
 export default {
-  name: 'SaveDataList',
+  name: "SaveDataList",
   components: {
     Navigation,
-    FixedBtn
+    FixedBtn,
   },
-  props:{
-    ResultNo : { required: true }
+  props: {
+    ResultNo: { required: true },
   },
-  setup(props){
+  setup(props) {
     const survey = useSurvey();
     const formatter = useFormatter();
 
-    const reactResult = reactive({Result:{}})
+    const reactResult = reactive({ Result: {} });
     const reactResultFormatted = reactive({
-      CreateAt: '',
-      BirthDay: '',
-      Gender: '',
+      CreateAt: "",
+      BirthDay: "",
+      Gender: "",
 
-      Age: '',
-      Families: '',
-      PayBackCount: '',
+      Age: "",
+      Families: "",
+      PayBackCount: "",
 
-      Phone: '',
-      UseAtomyYn: '',
+      Phone: "",
+      UseAtomyYn: "",
 
-      AmountPerYear: '',
-      PVPerYear: '',
+      AmountPerYear: "",
+      PVPerYear: "",
     });
 
     const item = computed(() => reactResult.Result);
 
-    onBeforeMount(()=>{
+    onBeforeMount(() => {
       survey.getResult(props.ResultNo).then((r) => {
-        if(r.data.Status === 1 && r.data.Data) {
-          const v = reactResult.Result = r.data.Data;
+        if (r.data.Status === 1 && r.data.Data) {
+          const v = (reactResult.Result = r.data.Data);
           reactResultFormatted.CreateAt = formatter.toDate(v.CreateAt);
-          reactResultFormatted.BirthDay = formatter.toDate(v.BirthDay, ['년 ','월 ','일']);
+          reactResultFormatted.BirthDay = formatter.toDate(v.BirthDay, [
+            "년 ",
+            "월 ",
+            "일",
+          ]);
           reactResultFormatted.Gender = formatter.toGender(v.Gender);
-          reactResultFormatted.Age = v.Age + '대';
-          reactResultFormatted.Families = v.Families + '인';
-          reactResultFormatted.PayBackCount = v.PayBackCount + '회';
+          reactResultFormatted.Age = v.Age + "대";
+          reactResultFormatted.Families = v.Families + "인";
+          reactResultFormatted.PayBackCount = v.PayBackCount + "회";
           reactResultFormatted.Phone = formatter.toPhone(v.Phone);
           reactResultFormatted.UseAtomyYn = formatter.toYn(v.UseAtomyYn);
-          reactResultFormatted.AmountPerYear = formatter.toPrice(v.AmountPerYear) + ' 원';
-          reactResultFormatted.PVPerYear = formatter.toPrice(v.PVPerYear) + ' PV';
-        }else{
-          alert('잠시후 다시 시도해 주세요.')
+          reactResultFormatted.AmountPerYear =
+            formatter.toPrice(v.AmountPerYear) + " 원";
+          reactResultFormatted.PVPerYear =
+            formatter.toPrice(v.PVPerYear) + " PV";
+        } else {
+          alert("잠시후 다시 시도해 주세요.");
           router.back();
         }
       });
     });
 
     function snsShare(title, url) {
-      if ( navigator.share ) {
+      if (navigator.share) {
         navigator.share({ title: title, url: url });
       } else {
-        alert('지원하지 않는 브라우저입니다.');
+        alert("지원하지 않는 브라우저입니다.");
       }
     }
 
-    function handleDelect(){
-      console.log('handleDelete');
-      if(confirm('해당 정보(들)을 삭제하시겠습니까?\n삭제하시면 저장된 리스트가 삭제되며\n복구가 불가능합니다.')){
+    function handleDelect() {
+      console.log("handleDelete");
+      if (
+        confirm(
+          "해당 정보(들)을 삭제하시겠습니까?\n삭제하시면 저장된 리스트가 삭제되며\n복구가 불가능합니다."
+        )
+      ) {
         survey.deletes([Number(props.ResultNo)]).then((r) => {
-          if(r.data.Status === 1 && r.data.Data){
+          if (r.data.Status === 1 && r.data.Data) {
             router.back();
-          }else{
-            alert('잠시후 다시 시도해 주세요.')
+          } else {
+            alert("잠시후 다시 시도해 주세요.");
           }
         });
       }
     }
 
-    function handleEdit(){
-      console.log('handleEdit');
-      router.push({path:'/SaveData', query:{ResultNo:props.ResultNo}})
+    function handleEdit() {
+      console.log("handleEdit");
+      router.push({ path: "/SaveData", query: { ResultNo: props.ResultNo } });
     }
 
     return {
@@ -160,9 +175,9 @@ export default {
       handleDelect,
       handleEdit,
       reactResultFormatted,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style lang="scss">
