eb5fd0e084649fcd0dfdd7af76ac3f808508f4b7
James <5000win@naver.com>
2022-03-15 14:38:30 +0900

    로딩페이지 : Page/Loading/index.vue 에러페이지 : Page/Error/index.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,78 +1,95 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 export const defaultRoutes = [
-    {
-        path: "/",
-        redirect: 'login',
-    },
-    {
-        path: "/login",
-        name: "Login",
-        component: () => import('@/Page/Login'),
-        meta: { unauthorized:true }
-    },
-    {
-        path: "/intro",
-        name: "intro",
-        component: () => import('@/Page/Intro'),
-    },
-    {
-        path: "/terms",
-        name: "terms",
-        component: () => import('@/Page/Terms'),
-    },
-    {
-        path: "/terms2",
-        name: "terms2",
-        component: () => import('@/Page/Terms/Terms2'),
-    },
-    {
-        path: "/qna",
-        name: "Qna",
-        component: () => import('@/Page/Account/QuestionPage'),
-    },
-    {
-        path: "/qna2",
-        name: "Qna2",
-        component: () => import('@/Page/Account/QuestionPage2'),
-    },
-    {
-        path: "/result",
-        name: "Result",
-        component: () => import('@/Page/Account/Result'),
-    },
-    {
-        path: "/result2",
-        name: "Result2",
-        component: () => import('@/Page/Account/Result2'),
-    },
-    {
-        path: "/AtomyProduct",
-        name: "AtomyProduct",
-        component: () => import('@/Page/AtomyProduct/AtomyProduct')
-    },
-    {
-        path: "/SaveData",
-        name: "SaveData",
-        component: () => import('@/Page/SaveData'),
-        props: true,
-    },
-    {
-        path: "/SaveDataList",
-        name: "SaveDataList",
-        component: () => import('@/Page/SaveData/SaveDataList'),
-    },
-    {
-        path: "/SaveDataView/:ResultNo",
-        name: "SaveDataView",
-        component: () => import('@/Page/SaveData/SaveDataView'),
-        props: true
-    },
+  {
+    path: "/",
+    redirect: "login",
+  },
+  {
+    path: "/loading",
+    name: "Loading",
+    component: () => import("@/Page/Loading"),
+    meta: { unauthorized: true },
+  },
+  {
+    path: "/Error",
+    name: "Error",
+    component: () => import("@/Page/Error"),
+    meta: { unauthorized: true },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/Page/Login"),
+    meta: { unauthorized: true },
+  },
+  {
+    path: "/intro",
+    name: "intro",
+    component: () => import("@/Page/Intro"),
+  },
+  {
+    path: "/terms",
+    name: "terms",
+    component: () => import("@/Page/Terms"),
+  },
+  {
+    path: "/terms2",
+    name: "terms2",
+    component: () => import("@/Page/Terms/Terms2"),
+  },
+  {
+    path: "/qna",
+    name: "Qna",
+    component: () => import("@/Page/Account/QuestionPage"),
+  },
+  {
+    path: "/qna2",
+    name: "Qna2",
+    component: () => import("@/Page/Account/QuestionPage2"),
+  },
+  {
+    path: "/result",
+    name: "Result",
+    component: () => import("@/Page/Account/Result"),
+  },
+  {
+    path: "/result2",
+    name: "Result2",
+    component: () => import("@/Page/Account/Result2"),
+  },
+  {
+    path: "/result3",
+    name: "Result3",
+    component: () => import("@/Page/Account/Result3"),
+  },
+  {
+    path: "/AtomyProduct",
+    name: "AtomyProduct",
+    component: () => import("@/Page/AtomyProduct/AtomyProduct"),
+  },
+  {
+    path: "/SaveData",
+    name: "SaveData",
+    component: () => import("@/Page/SaveData"),
+    props: true,
+  },
+  {
+    path: "/SaveDataList",
+    name: "SaveDataList",
+    component: () => import("@/Page/SaveData/SaveDataList"),
+  },
+  {
+    path: "/SaveDataView/:ResultNo",
+    name: "SaveDataView",
+    component: () => import("@/Page/SaveData/SaveDataView"),
+    props: true,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: defaultRoutes
+  history: createWebHistory(),
+  routes: defaultRoutes,
 });
 
 export default router;
