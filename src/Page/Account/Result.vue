128afe8d6940b7c000a1fc4278dcf385549d9149
Atomy James <jmkim@atomy.kr>
2022-04-06 17:09:07 +0900

    아이패드 디바이스 반응형 대응

--- src/Page/Account/Result.vue
+++ src/Page/Account/Result.vue
@@ -47,33 +47,35 @@
               </tbody>
             </table>
           </div>
-          <table class="table-capsule">
-            <caption>
-              구매금액 및 누적 PV
-            </caption>
-            <colgroup>
-              <col style="width: 40%" />
-              <col />
-            </colgroup>
-            <thead>
-              <tr>
-                <th scope="col" colspan="2">
-                  <img :src="require(`@/assets/image/i-question.svg`)" />
-                  내가 연간 받을 수 있는 혜택은?
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">캐쉬백 횟수</th>
-                <td>약 {{ payback.count }}회(최소)</td>
-              </tr>
-              <tr>
-                <th scope="row">총 캐쉬백 금액</th>
-                <td>약 {{ formatter.toPrice(payback.amount) }}원</td>
-              </tr>
-            </tbody>
-          </table>
+          <div class="table-capsule-wrap">
+            <table class="table-capsule">
+              <caption>
+                구매금액 및 누적 PV
+              </caption>
+              <colgroup>
+                <col style="width: 40%" />
+                <col />
+              </colgroup>
+              <thead>
+                <tr>
+                  <th scope="col" colspan="2">
+                    <img :src="require(`@/assets/image/i-question.svg`)" />
+                    내가 연간 받을 수 있는 혜택은?
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">캐쉬백 횟수</th>
+                  <td>약 {{ payback.count }}회(최소)</td>
+                </tr>
+                <tr>
+                  <th scope="row">총 캐쉬백 금액</th>
+                  <td>약 {{ formatter.toPrice(payback.amount) }}원</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
           <div class="d-grid gap-3">
             <button class="btn btn-outline-primary block">
