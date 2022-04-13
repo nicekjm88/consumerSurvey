c59a898496692579b3a140903fc50cfef1a97d41
ibro-jmk <jmbaragi@ibrogroup.co.kr>
2022-04-13 16:48:51 +0900

    web 에서 공유 페이지만 접근 가능하게 변경

--- src/composables/api/common/define.js
+++ src/composables/api/common/define.js
@@ -2,5 +2,5 @@ export const _ATOMY_API_TOKEN_NAME = 'Atomy-Api-Token';
 export const _ATOMY_USER_TOKEN_NAME = 'Atomy-User-Token';
 export const _JISA_CODE = 'kr';
 export const _ATOMY_URL = '/atomy';
-export const _ATOMY_PROXY = 'https://test-dev.atomy.com/apiglobal';
-//export const _ATOMY_PROXY = 'https://api.atomy.com/apiglobal';
+//export const _ATOMY_PROXY = 'https://test-dev.atomy.com/apiglobal';
+export const _ATOMY_PROXY = 'https://api.atomy.com/apiglobal';
--- src/permission.js
+++ src/permission.js
@@ -1,9 +1,12 @@
 import router from './router'
 import useUserManager from "@/store/user-manager";
+import {Capacitor} from "@capacitor/core";
 
 router.onError(async () => {
     // none
 });
+
+const web_pass_filter = ['notFound','Error','Loading', 'ShareView', 'ShareAtomyProduct']
 
 router.beforeEach(async (to, from, next) => {
     //권한 처리
@@ -11,27 +14,35 @@ router.beforeEach(async (to, from, next) => {
     const { unauthorized, role } = to.meta;
     let next_path = undefined;
 
-    if(to.path === '/login'){
-        //로그인 페이지로 들어왔을때 로그인 가능하면 인트로 페이지로 이동
-        if(await userManager.checkLogin()) next_path = '/intro';
-    }else if(!unauthorized){
-        //권한 확인 후 권한 없으면 로그인 페이지로 이동
-        if(!await userManager.checkLogin()){
-            next_path = '/'
+    if(Capacitor.getPlatform() === 'web'){
+        if(web_pass_filter.some(x => x === to.name)){
+            next();
+        }else {
+            next('/error');
         }
-    }
-
-    if(next_path === undefined && role) {
-        const r = role.filter((x) => x === userManager.identity.role);
-        if(r.length === 0){
-            next_path = '/error'
+    }else {
+        if (to.path === '/login') {
+            //로그인 페이지로 들어왔을때 로그인 가능하면 인트로 페이지로 이동
+            if (await userManager.checkLogin()) next_path = '/intro';
+        } else if (!unauthorized) {
+            //권한 확인 후 권한 없으면 로그인 페이지로 이동
+            if (!await userManager.checkLogin()) {
+                next_path = '/';
+            }
         }
-    }
 
-    if(next_path === undefined) {
-        next()
-    } else {
-        next(next_path)
+        if (next_path === undefined && role) {
+            const r = role.filter((x) => x === userManager.identity.role);
+            if (r.length === 0) {
+                next_path = '/error';
+            }
+        }
+
+        if (next_path === undefined) {
+            next();
+        } else {
+            next(next_path);
+        }
     }
 });
 
